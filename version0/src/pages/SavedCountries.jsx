082f8e24ba21053c58c715178f2f16d5cade34b9
Hailey <137557225+officiallyhailey@users.noreply.github.com>
2026-05-11 16:03:45 -0400

    seperating versions into folders

---
+++ version0/src/pages/SavedCountries.jsx
@@ -0,0 +1,22 @@
+
+function SavedCountries() {
+    return (
+        <div className="saved-countries">
+            <h1>My Saved Countries</h1>
+
+            {/* TODO: Add filter to display saved countries using CountryCard component
+            Possibly add heart icon to CountryCard to allow users to save countries to this page */}
+            
+
+            <h1>Welcome back, {name}!</h1>
+            <p> My Profile</p>
+
+            {/* TODO: add profile form component 
+     show saved data
+            */}
+
+        </div>
+    );
+}
+
+export default SavedCountries;
